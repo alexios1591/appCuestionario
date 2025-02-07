40a8a93afb08d274f27c9e2a33fdbd085c541465
lalechelml <ramos.joshelin20@gmail.com>
2025-02-07 13:40:39 -0500

    Update CuestionarioGuard to navigate to home for non-authenticated users

--- src/app/guards/cuestionario.guard.ts
+++ src/app/guards/cuestionario.guard.ts
@@ -8,10 +8,9 @@ export class CuestionarioGuard implements CanActivate {
   constructor(private router: Router) {}
 
   canActivate(): boolean {
-    const user = JSON.parse(localStorage.getItem('user') || 'null');
-
-    if (!user || user.rol !== 'admin') {
-      this.router.navigate(['/admin']);
+    const user = localStorage.getItem('usuario');
+    if (!user) {
+      this.router.navigate(['/']);
       return false;
     }
     return true;
