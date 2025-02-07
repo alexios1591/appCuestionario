88d2f758eb1e60a3d024f5a2a02f61c73a386083
lalechelml <ramos.joshelin20@gmail.com>
2025-02-07 13:13:07 -0500

    Add CuestionarioGuard to user and rol routes in admin module

--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -1,15 +1,27 @@
-import { Routes } from "@angular/router";
-import { LayoutComponent } from "./layout/layout.component";
-import { HomeComponent } from "./home/home.component";
-import { UserComponent } from "./user/user.component";
-import { AuthGuard } from "../guards/auth.guard";
-import { RolComponent } from "./rol/rol.component";
+import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { AuthGuard } from '../guards/auth.guard';
+import { CuestionarioGuard } from '../guards/cuestionario.guard';
+import { RolComponent } from './rol/rol.component';
 
 export const ADMIN_ROUTES: Routes = [
-    {path: '', component: LayoutComponent, children:[
-        {path: '', component: HomeComponent, canActivate: [AuthGuard]},
-        {path: 'user', component: UserComponent, canActivate: [AuthGuard] },
-        {path: 'rol', component: RolComponent, canActivate: [AuthGuard] },
-
-    ]},
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+      {
+        path: 'user',
+        component: UserComponent,
+        canActivate: [AuthGuard, CuestionarioGuard],
+      },
+      {
+        path: 'rol',
+        component: RolComponent,
+        canActivate: [AuthGuard, CuestionarioGuard],
+      },
+    ],
+  },
 ];
