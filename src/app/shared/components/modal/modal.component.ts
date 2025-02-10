0f973f3ee7766018840646ecf4292ed7d8a660ac
lalechelml <ramos.joshelin20@gmail.com>
2025-02-09 21:08:09 -0500

    Add PDF icon to modal component and improve code structure

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,8 +1,9 @@
 import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faXmark, faFilePdf } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CommonModule } from '@angular/common';
 import { CuestionarioService } from '../../../api/cuestionario.service';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-modal',
@@ -12,20 +13,23 @@ import { CuestionarioService } from '../../../api/cuestionario.service';
 export class ModalComponent {
   @Input() cliente: any;
   @Output() close = new EventEmitter<void>();
-  preguntas: any
+  preguntas: any;
 
-  cuestionaroService = inject(CuestionarioService)
+  faFilePdf = faFilePdf;
+  environment = environment;
 
-  ngOnInit(){
+  cuestionaroService = inject(CuestionarioService);
+
+  ngOnInit() {
     this.cuestionaroService.getById(this.cliente.CodClie).subscribe(
       (data) => {
-        this.preguntas = data.cuestionario
-        console.log(data)
+        this.preguntas = data.cuestionario;
+        console.log(data);
       },
       (error) => {
-        console.log(error)
+        console.log(error);
       }
-    )
+    );
   }
 
   closeModal() {
@@ -43,10 +47,9 @@ export class ModalComponent {
       this.preguntas?.Pre7,
       this.preguntas?.Pre8,
       this.preguntas?.Pre9,
-      this.preguntas?.Pre11
+      this.preguntas?.Pre11,
     ].reduce((sum, value) => sum + (Number(value) || 0), 0);
   }
 
-
-  faXMark = faXmark
+  faXMark = faXmark;
 }
